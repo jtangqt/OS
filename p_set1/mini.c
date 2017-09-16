a358aa3b5da2a99591ad63a5442483f8b39152c7
jtangqt <ajasmineflower@gmail.com>
2017-09-16 10:02:10 -0400

    did all cases for -o and -b

--- p_set1/mini.c
+++ p_set1/mini.c
@@ -8,6 +8,7 @@ int main(int argc, char **argv){
 
 	char *bit_val = 0;
 	char *out_file = NULL; 
+	char *in_file = NULL;
 	int c; 
 
 	opterr = 0; 
@@ -16,6 +17,9 @@ int main(int argc, char **argv){
 		switch(c){
 			case 'b':
 				bit_val = optarg; 
+				//check if this a number
+				// if atoi(bitval) != 1 then error message
+					// if bitval is too large/too small, then error message
 				break; 
 			case 'o':
 				out_file = optarg; 
@@ -23,10 +27,27 @@ int main(int argc, char **argv){
 			case '?':
 				if (optopt == 'b' || optopt == 'o')
 					fprintf(stderr, "Option -%c requires an argument", optopt);
+				else
+					fprintf(stderr, "%c", optopt);
+					//in_file = optopt;
+					//if in_file does not exist, error message
 					return 1; 
 			default: 
 				abort(); 
 		}
+
 		printf("%s%s", bit_val, out_file);
-		return 0; 
+		// if(out_file ! = NULL){
+		// 	//print the input in the out_file
+		// }
+		// else{
+		// 	//print the input in std. output
+		// }
+
+		// return 0; 
 }
+
+//the operation that was being attempted
+//the object being operated on (the file name)
+//other relevant parameters
+//the error returned (strerror(errno))
