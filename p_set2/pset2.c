15626136446514c1b1634807eba6d498c5d709bb
Jasmine <ajasmineflower@gmail.com>
2017-10-20 10:54:15 -0400

    added newest version

--- p_set2/pset2.c
+++ p_set2/pset2.c
@@ -19,25 +19,6 @@ void error_message(const char *message, const char *error_val, const char *file_
 	exit(-1);
 }
 
-void message(char *next_path, int n_link, char *link, char *read, char *duplicate){
-	if(!n_link){
-		if(!duplicate)
-			fprintf(stderr, "%s\t%s to target", next_path, link); 
-		else  
-			fprintf(stderr, "%s\t%s to duplicate at %s", next_path, link, duplicate); 
-	}
-	else{
-		fprintf(stderr, "%s\tduplicate to target", next_path); 
-		if(n_link > 1)	
-			fprintf(stderr, " (n_link = %i)", n_link); 
-	}
-	if(read)	
-		fprintf(stderr, ", %s to read by other\n", read); 
-	else
-		fprintf(stderr, "\n");
-
-}
-
 //take all elements in the directory and check if they are identical to the file given
 //if they are identical, then print out all of its information 
 //pathname, linkcount > 1 (hardlink, symlink, distinct -> nlink), and read by users? 
@@ -49,9 +30,7 @@ int main(int argc, char **argv){
 		error_message("ERROR! Too many input arguments. Please follow this format\nfinddir [direction to input file] [starting directory]\n", 0, 0); 
 	}
 
-	chdir(argv[2]);
-	getcwd(cwd, sizeof(cwd));
-	search_tree(argv[1], cwd);
+	search_tree(argv[1], argv[2]);
 
 }
 
@@ -66,19 +45,20 @@ void search_tree(char * file_path, char *s_path){
 
 	// going through each layer	
 	if(!(dir = opendir(s_path))) //check for error in opening a directory
-		error_message("ERROR: Could not open.", strerror(errno), s_path);
+		error_message("ERROR: Could not open.", strerror(errno), s_path);		
+
+	char *next_path = (char *)malloc(strlen(s_path) + strlen(de -> d_name) + 2); 
+	sprintf(next_path, "%s/%s", s_path, de -> d_name); 
+	if(!lstat(next_path, &st))
+		error_message("ERROR: Could not lstat.", strerror(errno), next_path);
+	
 	while(de = readdir(dir)){
-		char *duplicate = NULL, *read = NULL;
-		char buf[512] = {};
+		char * buf; 
 		int n_link = 0;
 
 		if(!strcmp(de -> d_name, "..") || !strcmp(de -> d_name, "."))
 			continue;	
 
-		char *next_path = (char *)malloc(strlen(s_path) + strlen(de -> d_name) + 2); 
-		sprintf(next_path, "%s/%s", s_path, de -> d_name); 
-		lstat(next_path, &st);
-		
 		if((st.st_mode & S_IFMT) != S_IFDIR && (st.st_mode & S_IFMT) != S_IFREG && (st.st_mode & S_IFMT) != S_IFLNK)
 			continue; 
 		
@@ -86,18 +66,65 @@ void search_tree(char * file_path, char *s_path){
 			search_tree(file_path, next_path);
 		
 		else if((st.st_mode & S_IFMT) == S_IFREG){ //if regular file 
+			char *read = NULL; 
+			
 			if(st.st_mode & S_IROTH) //if it's readable by others
 				read = "able";
 			else
 				read = "not able";
 			
-			fprintf(stderr, "%s, %s, %lu, %lu\n", file_path, next_path, st.st_ino, file_st.st_ino);
-			if(!(st.st_ino == file_st.st_ino)){ //this indicates a duplicate
-				if(st.st_size != file_st.st_size) //check to see if the sizes match
-					continue; 
-				file1 = fopen(file_path, "r"); //now compare actual contents
-				file2 = fopen(next_path, "r"); 
+			//case 1: inodes match
+			if(st.st_ino == file_st.st_ino){
+				if(st.st_nlink == 1)
+					fprintf(stderr, "%s\t original file found, %s to read by others\n", next_path, read);
+				else if (st.st_nlink > 1)
+					fprintf(stderr, "%s\t hardlink to target found (n_link = %lu), %s to read by others\n", next_path, st.st_nlink, read);
+				continue; 
+			}
+
+			//case 2: inodes don't match - compare size of file first 
+			if(st.st_size != file_st.st_size)
+				continue;
 			
+			//now compare actual contents
+			file1 = fopen(file_path, "r"); 
+			file2 = fopen(next_path, "r");			
+			do {
+				a = fgetc(file1);
+				b = fgetc(file2);
+				if (a != b){
+					fclose(file1);
+					fclose(file2); 
+					break;
+				}
+			} while(a != EOF && b != EOF); 
+			if (a != b){
+				fclose(file1);
+				fclose(file2); 
+				continue;	
+			}
+
+			fprintf(stderr, "%s\n duplicate to target found, %s to read by others\n", next_path, read);
+			
+			fclose(file1);
+			fclose(file2); 
+		}
+		
+		else{ //if symlink
+			char *duplicate = NULL; 
+
+			//case 1: inodes match		
+			if(file_st.st_ino == st.st_ino)
+				fprintf(stderr, "%s\tsymlink to target found\n", next_path);
+			//case 2: inodes don't match
+			else{
+				//compare size of file
+				if(st.st_size != file_st.st_size)
+					continue;
+
+				//now compare actual contents
+				file1 = fopen(file_path, "r"); 
+				file2 = fopen(next_path, "r");			
 				do {
 					a = fgetc(file1);
 					b = fgetc(file2);
@@ -105,28 +132,19 @@ void search_tree(char * file_path, char *s_path){
 						break;
 				} while(a != EOF && b != EOF); 
 				if (a != b)
-					continue; 
-				duplicate = "duplicate";
-				n_link = file_st.st_nlink; 
-			}
-		
-			message(next_path, n_link, "hard link", read, duplicate);
-			fclose(file1);
-			fclose(file2); 
-		
-		}
-		
-		else{
-			readlink(next_path, buf, sizeof(buf)-1);
+					continue;	
+			
+				fclose(file1);
+				fclose(file2); 
 
-			if(strcmp(buf, file_path)){
-				char *check_path = (char *)malloc(strlen(s_path) + strlen(buf) + 2); 
-				sprintf(check_path, "%s/%s", s_path, buf); 
-				duplicate = check_path;
-			}
+				buf = (char*)malloc(st.st_size+1); 
+				readlink(next_path, buf, sizeof(buf)+1);
 
-			message(next_path, 0, "sym link", 0, duplicate); 
-		
+				fprintf(stderr, "%s\tsymlink to duplicate found (%s)\n", next_path, buf);
+
+			}
+			
+			free(buf); 
 		}
 	}
 	closedir(dir);
