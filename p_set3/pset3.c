52250c9aebe64fd95fa579d6634e994c0e232558
jtangqt <tang@cooper.edu>
2017-10-29 21:00:27 -0400

    testing for shell script

--- p_set3/pset3.c
+++ p_set3/pset3.c
@@ -104,9 +104,10 @@ int processing(char *input){
 
 	argv_new[new] = NULL; 
 
-	struct timeval s_time; 
+	struct timeval s_time;
+	struct timeval e_time; 
 	if(gettimeofday(&s_time, NULL)){
-		error_message("ERROR: could not get start time.", strerror(errno), 0);
+		return error_message("ERROR: could not get start time.", strerror(errno), 0);
 	}
 
 	if(argv_new[0] != NULL && !strcmp(argv_new[0], "cd")){ //directory
@@ -114,11 +115,17 @@ int processing(char *input){
 			return error_message("ERROR: no directory path was specified.", strerror(errno), 0); 
 		char *new_dir = argv_new[1]; 
 		if(chdir(new_dir)){
-			return error_message("ERROR: could not open directory (perhaps try relative pathname). Error message: %s. Path name: %s.\n", strerror(errno), new_dir);
+			return error_message("ERROR: could not open directory (perhaps try relative pathname).", strerror(errno), new_dir);
 		}
 		return 0; 
-	}	
+	}
 
+	if(argv_new[0] != NULL && !strcmp(argv_new[0], "exit")){
+		if(!argv_new[1])
+			exit(0); 
+		else
+			exit(atoi(argv_new[1]));
+	} 
 
 	pid_t pid = fork(); 
 	if(pid == -1){
@@ -129,36 +136,50 @@ int processing(char *input){
 			fprintf(stderr, "in in\n");
 			redirect(0, O_RDONLY, in);
 		}
-		//fprintf(stderr, "debug1"); 
+
 		if(a_out != NULL){
 			fprintf(stderr, "in a_out\n");
 			redirect(1, O_RDWR| O_APPEND| O_CREAT, a_out); 
 		}
-		//fprintf(stderr, "debug2");
 		else if(t_out != NULL){
 			fprintf(stderr, "in t_out\n");
 			 redirect(1, O_RDWR| O_TRUNC| O_CREAT, t_out); 
 		}
-		//fprintf(stderr, "debug3"); 
-		if(a_err != NULL){
+
+		if(a_err != NULL){ 
 			fprintf(stderr, "in a_err\n");
 			redirect(2, O_RDWR| O_APPEND| O_CREAT, a_err); 
 		}
-	//	fprintf(stderr, "debug4"); 
 		else if(t_err != NULL){
 			fprintf(stderr, "in t_err\n");
 			redirect(2, O_RDWR| O_TRUNC| O_CREAT, t_err); 
 		}
+		
 		if(execvp(argv_new[0], argv_new) < 0){
-			return error_message("ERROR: execvp() failed.", strerror(errno), argv_new[0]); 	
+			error_message("ERROR: execvp() failed.", strerror(errno), argv_new[0]);
+			exit(1); 
 		}
 	}
 	else{
-		
+		int wstatus = 0;
+		struct timeval d_time; 
+		struct rusage ru; 
+		if(wait4(pid, &wstatus, 0, &ru)<0){
+			error_message("ERROR: wait4() failed.", strerror(errno), 0); 
+			exit(1); 
+		}
+		else{
+			if(gettimeofday(&e_time, NULL)){
+				return error_message("ERROR: could not get end time.", strerror(errno), 0);
+			}
+			timersub(&e_time, &s_time, &d_time);
+			fprintf(stderr, "Command: %sReturn Code: %i\n", input, WEXITSTATUS(wstatus)); 
+			fprintf(stderr, "Real time: %01d.%03d\n", d_time.tv_sec, d_time.tv_usec); 
+			fprintf(stderr, "User time: %01d.%03d\n", ru.ru_utime.tv_sec, ru.ru_utime.tv_usec); 
+			fprintf(stderr, "System time: %01d.%03d\n", ru.ru_stime.tv_sec, ru.ru_stime.tv_usec); 
+		}
 	}
-
 	return 0;
-
 }
 
 int main(int argc, char **argv){
---
+++ p_set3/testme.sh
@@ -0,0 +1,10 @@
+#!/
+#This is an example of a shell script that your shell must execute, notice that lines starting with # sign are ignored as comments!
+#Let's say this here file is called testme.sh. You created it with vi testme.sh ; chmod +x testme.sh# You invoked it with ./testme.sh
+cat >cat.out
+# at this point, type some lines at the keyboard, then create an EOF
+#your shell invoked the system cat command with output redirected to cat.out
+cat cat.out
+#you better see the lines that you just typed!
+exit 123
+#after your script exits, type echo $? from the UNIX system shell, the value should be 123. Since your shell just exited, the following bogus command should never be seen
