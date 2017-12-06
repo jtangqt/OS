52f750e45b0da89d07c55a10d3a05a0b8b28f4fa
Jasmine <ajasmineflower@gmail.com>
2017-12-06 18:55:05 -0500

    almost halfway with launcher

--- 4/launcher.c
+++ 4/launcher.c
@@ -5,6 +5,52 @@
 #include <errno.h>
 #include <fcntl.h>
 
-int main(){
-	
+
+int error_message(const char *message, const char *error_val, const char *file_val){
+	if(!file_val){
+		if(!error_val)
+			fprintf(stderr, "%s\n", message);
+		else
+			fprintf(stderr, "%s Error value: %s\n", message, error_val);
+	}
+	else
+		fprintf(stderr, "%s Error value: %s. Name: %s\n", message, error_val, file_val);
+	exit(EXIT_FAILURE);
+}
+
+int main(int argc, char **argv){
+	pid_t pid_1, pid_2, pid_3;
+	int wstatus; 
+	int pipe1[2], pipe2[2];
+
+	if(argc > 2){
+		error_message("ERROR: Please follow this format\n./launcher [number]", strerror(errno), 0); 
+	}
+	if(pipe(pipe1) == -1)
+		error_message("ERROR: Pipe1", strerror(errno), 0);
+
+	//first fork
+	pid_1 = fork(); 
+	if(pid_1 == -1)
+		error_message("ERROR: could not fork to create a new process.", strerror(errno), 0);
+	else if (!pid_1)
+	else{
+		if(pipe(pipe2) == -1)
+			error_message("ERROR: Pipe2", strerror(errno), 0);
+
+		//second fork
+		pid_2 = fork();
+		if(pid_2 == -1)
+		else if (!pid_2)
+		else{
+
+			//third fork
+			pid_3 = fork(); 
+			if(pid_3 == -1)
+			else if (!pid_3)
+			else{
+
+			}
+		}
+	}
 }
