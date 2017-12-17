ca59591bdcc78f0aca69bb414eba2f54a622ff9c
Jasmine <ajasmineflower@gmail.com>
2017-12-16 22:30:21 -0500

    started the tests

--- 5/pset5.c
+++ 5/pset5.c
@@ -4,13 +4,42 @@
 #include <string.h>
 #include <errno.h>
 #include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
-int test1(){}
-int test2(){}
-int test3(){}
-int test4(){}
-int test5(){}
-int test6(){}
+int create_fd(int length){
+	int fd; 
+
+	if((fd = open("file.txt", O_WRONLY | O_CREAT | O_TRUNC, 0666)) == -1)
+		fprintf(stderr, "ERROR MESSAGE");
+
+	return fd; 
+}
+
+int test_1(){
+	printf("Executing Test #1 (write to r/o mmap): ");
+}
+
+int test_23(int flags){
+	if(flag = MAP_SHARED)
+		printf("Executing Test #2 (writing to MAP_SHARED): ");
+	else if(flag = MAP_PRIVATE)
+		printf("Executing Test #3 (writing to MAP_PRIVATE): ");
+	else
+		fprintf(stderr, "Invalid command for test 2 or 3")
+}
+
+int test_4(){
+	printf("Executing Test #4 (writing beyond the edge): ");
+}
+
+int test_5(){
+	printf("Executing Test #5 (writing into a hole): ");
+}
+
+int test_6(){
+	printf("Executing Test #6 (reading beyond the edges): ");
+}
 
 int main(int argc, char **argv){
 	if(argc > 2)
@@ -18,17 +47,17 @@ int main(int argc, char **argv){
 	else
 		switch(atoi(argv[1])){
 			case 1:
-				test1();
+				test_1();
 			case 2:
-				test2();
+				test_23(MAP_SHARED);
 			case 3:
-				test3();
+				test_23(MAP_PRIVATE);
 			case 4:
-				test4();
+				test_4();
 			case 5:
-				test5();
+				test_5();
 			case 6:
-				test6(); 
+				test_6(); 
 			default:
 				fprintf(stderr, "ERROR MESSAGE");
 		}
