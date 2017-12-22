fb09f7c27432c22155e2e67e877390817db82c7a
Jasmine <ajasmineflower@gmail.com>
2017-12-22 23:36:40 +0800

    finished 1, 2, 3

--- 5/pset5.c
+++ 5/pset5.c
@@ -11,26 +11,38 @@
 #include <signal.h>
 
 void sig_handler(int sig){
-	fprintf(stderr, "#%d, %s received!\n", sig, strsignal(sig));
+	fprintf(stderr, "Signal %s received!\n", strsignal(sig));
 	exit(sig);
 }
 
 int create_fd(int length){
-	int fd;
+	int fd; 
 	char buf[1];
+
 
 	if((fd = open("file.txt", O_RDWR|O_CREAT|O_TRUNC, 0666)) < 0){
 		fprintf(stderr, "ERROR MESSAGE: ");
 		exit(255);
 	}
 	srand(time(NULL));
+
 	for(int i = 0; i < length; i++){
-		buf[0] = rand()%10; 
-		if(write(fd, buf, 1) < 0){
-			fprintf(stderr, "ERROR MESSAGE: ");
-			exit(255);
+		if((rand() % 36) <= 10){
+			sprintf(buf, "%i", rand()%10);
+			if(write(fd, buf, 1) < 0){
+				fprintf(stderr, "ERROR MESSAGE: ");
+				exit(255);
+			}
+		}
+		else{
+			buf[0] = rand()%26 + 'A'; 
+			if(write(fd, buf, 1) < 0){
+				fprintf(stderr, "ERROR MESSAGE: ");
+				exit(255);
+			}	
 		}
 	}
+
 	return fd; 
 }
 
@@ -53,13 +65,14 @@ int test_1(){
 	printf("Executing Test #1 (write to r/o mmap): \n");
 	int fd = create_fd(length);	
 
-	if((map = mmap(NULL, length, PROT_WRITE, MAP_SHARED, fd, 0)) == MAP_FAILED){
+	if((map = mmap(NULL, length, PROT_READ, MAP_SHARED, fd, 0)) == MAP_FAILED){
 		fprintf(stderr, "ERROR MESSAGE  could not map, %s\n", strerror(errno));
 		exit(255);
 	}
 
-	printf("map[3] = \'%d\'\nwriting a \'%d\'\n", map[3], map[4]);
+	printf("map[3] = \'%c\'\nwriting a \'%c\'\n", map[3], map[4]);
 	map[3] = map[4];
+	printf("After write, map[3] returned \'%c\'\n", map[3]);
 	map_close_error(length, fd, map);
 
 	exit(0);
@@ -70,11 +83,17 @@ int test_23(int flag){
 	char *map;
 	size_t length = 4096; 
 	char buf[1];
+	char *out_flag;
+	int private, offset = 6;  
 
-	if(flag = MAP_SHARED)
+	if(flag == MAP_SHARED){
 		printf("Executing Test #2 (writing to MAP_SHARED): \n");
-	else if(flag = MAP_PRIVATE)
+		out_flag = "MAP_SHARED";
+	}
+	else if(flag == MAP_PRIVATE){
 		printf("Executing Test #3 (writing to MAP_PRIVATE): \n");
+		out_flag = "MAP_PRIVATE";
+	}
 	else
 		fprintf(stderr, "Invalid command for test 2 or 3");
 
@@ -84,23 +103,24 @@ int test_23(int flag){
 		exit(255);
 	}
 
-	int offset = 6; 
-	printf("map[%d] = \'%d\'\nwriting a new value: 9", offset, map[offset]);
-	map[offset] = 9;
+	printf("map[%d] = \'%c\'\nwriting a new value: J\n", offset, map[offset]);
+	map[offset] = 'J';
 
 	if(lseek(fd, offset, SEEK_SET) < 0)
 		fprintf(stderr, "ERROR MESSAGE LSEEK:");
 	if(read(fd, buf, 1) < 0)
 		fprintf(stderr, "ERROR MESSAGE READ: ");
 
-	printf("map[%d] returned %d", offset, map[offset]);
+	if(buf[0] == 'J')
+		private = 1;
+	else
+		private = 0;
+
+	printf("Updated map[%d] with \'%c\' for %s. Map %s private.\n", offset, map[offset], out_flag, private ? "is not": "is");
 
 	map_close_error(length, fd, map);
 
-	if(buf[0] == 9)
-		exit(1);
-	else
-		exit(0);
+	private ? exit(1) : exit(0);
 
 }
 
